15c26e956d48c373920de47de903d1bdb20e0725
mcounts <mcounts@quoininc.com>
2018-07-20 14:52:14 -0400

    #6 solution canvas with static image

--- client/wizard/index.js
+++ client/wizard/index.js
@@ -7,6 +7,7 @@ const quesitonnaireIntroTemplate = require('./questionnaire-intro.hbs');
 const quesitonnairePrivacyTemplate = require('./questionnaire-privacy.hbs');
 const quesitonnaireDescriptionTemplate = require('./questionnaire-description.hbs');
 const quesitonnaireLevelsTemplate = require('./questionnaire-levels.hbs');
+const quesitonnaireSolutionCanvasTemplate = require('./quesitonnaire-solution-canvas.hbs');
 
 (($) => $(document).ready(() => {
     const loader = warpjsUtils.toast.loading($, "Page is loading");
@@ -72,6 +73,11 @@ const quesitonnaireLevelsTemplate = require('./questionnaire-levels.hbs');
                         $(document).on('click', '.levels-next', (event) => {
                             event.preventDefault();
                             result.data.levelOfDetail = $("input[name='questionnaire-level'][checked='checked']").val();
+                            $('.ipt-body').html(quesitonnaireSolutionCanvasTemplate());
+                        });
+
+                        $(document).on('click', '.solution-canvas-next', (event) => {
+                            event.preventDefault();
                         });
                     })
                 ;
