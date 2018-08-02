a4cf906917b74874044f5fac63e79e37cb554169
mcounts <mcounts@quoininc.com>
2018-08-02 11:12:19 -0400

    warpjs-ipt-plugin #10 dynamic initial views

--- client/wizard/index.js
+++ client/wizard/index.js
@@ -6,10 +6,8 @@ const errorTemplate = require('./../error.hbs');
 const template = require('./../template.hbs');
 const questionnaireTemplate = require('./questionnaire.hbs');
 const questionnaireIntroTemplate = require('./questionnaire-intro.hbs');
-const questionnairePrivacyTemplate = require('./questionnaire-privacy.hbs');
 const questionnaireDescriptionTemplate = require('./questionnaire-description.hbs');
 const questionnaireLevelsTemplate = require('./questionnaire-levels.hbs');
-const questionnaireSolutionCanvasTemplate = require('./questionnaire-solution-canvas.hbs');
 const questionnaireIterationTemplate = require('./questionnaire-iterations.hbs');
 const questionnaireEndTemplate = require('./questionnaire-end.hbs');
 
@@ -55,8 +53,6 @@ const questionnaireEndTemplate = require('./questionnaire-end.hbs');
                 placeholder.html(errorTemplate(result.data));
             } else {
                 return Promise.resolve()
-                    .then(() => questionnaireIntroTemplate())
-                    .then((content) => $('.ipt-body').html(content))
                     .then(() => warpjsUtils.documentReady($))
                     .then(() => {
                         function assignDetailLevelSelected() {
@@ -64,78 +60,21 @@ const questionnaireEndTemplate = require('./questionnaire-end.hbs');
                             $("input[name='questionnaire-level'][value='" + detailLevel + "']").attr('checked', 'checked');
                         }
 
-                        $(document).on('click', '.intro-next', (event) => {
-                            event.preventDefault();
-                            $('.ipt-body').html(questionnairePrivacyTemplate());
-                            progress = 1 / progressTotal * 100;
-                            $('.ipt .progress-bar').css('width', progress + '%');
-                        });
-                        $(document).on('click', '.privacy-back', (event) => {
-                            event.preventDefault();
-                            progress = 0;
-                            $('.ipt-body').html(questionnaireIntroTemplate());
-                            $('.ipt .progress-bar').css('width', progress + '%');
-                        });
-                        $(document).on('click', '.privacy-next', (event) => {
-                            event.preventDefault();
-                            $('.ipt-body').html(questionnaireDescriptionTemplate({projectName: result.data.projectName, projectStatus: result.data.projectStatus, mainContact: result.data.mainContact}));
-                            progress = 2 / progressTotal * 100;
-                            $('.ipt .progress-bar').css('width', progress + '%');
-                        });
-                        $(document).on('click', '.description-back', (event) => {
-                            event.preventDefault();
+                        $(document).on('click', '.description-back', () => {
                             result.data.projectName = $('#project-name').val();
                             result.data.mainContact = $('#main-contact').val();
                             result.data.projectStatus = $('#project-status').val();
-                            $('.ipt-body').html(questionnairePrivacyTemplate());
-                            progress = 1 / progressTotal * 100;
-                            $('.ipt .progress-bar').css('width', progress + '%');
                         });
-                        $(document).on('click', '.description-next', (event) => {
-                            event.preventDefault();
-                            // const url = result.data._links.self.href;
-                            // const data = {
-                            //     projectName: $('#project-name').val(),
-                            //     mainContact: $('#main-contact').val(),
-                            //     projectStatus: $('#project-status').val()
-                            // };
-
+                        $(document).on('click', '.description-next', () => {
                             result.data.projectName = $('#project-name').val();
                             result.data.mainContact = $('#main-contact').val();
                             result.data.projectStatus = $('#project-status').val();
-
-                            // Promise.resolve()
-                            //     .then(() => warpjsUtils.proxy.patch($, url, data))
-                            //     .then((update) => {
-                            // })
-
-                            $('.ipt-body').html(questionnaireLevelsTemplate({level: result.data.detailLevel}));
-                            progress = 3 / progressTotal * 100;
-                            $('.ipt .progress-bar').css('width', progress + '%');
-                            assignDetailLevelSelected();
-                            styleRadio();
                         });
-                        $(document).on('click', '.levels-back', (event) => {
-                            event.preventDefault();
+                        $(document).on('click', '.levels-back', () => {
                             result.data.detailLevel = $("input[name='questionnaire-level'][checked='checked']").val();
-                            $('.ipt-body').html(questionnaireDescriptionTemplate({projectName: result.data.projectName, projectStatus: result.data.projectStatus, mainContact: result.data.mainContact}));
-                            progress = 2 / progressTotal * 100;
-                            $('.ipt .progress-bar').css('width', progress + '%');
                         });
-                        $(document).on('click', '.levels-next', (event) => {
-                            event.preventDefault();
+                        $(document).on('click', '.levels-next', () => {
                             result.data.detailLevel = $("input[name='questionnaire-level'][checked='checked']").val();
-                            $('.ipt-body').html(questionnaireSolutionCanvasTemplate());
-                            progress = 4 / progressTotal * 100;
-                            $('.ipt .progress-bar').css('width', progress + '%');
-                        });
-                        $(document).on('click', '.solution-canvas-back', (event) => {
-                            event.preventDefault();
-                            $('.ipt-body').html(questionnaireLevelsTemplate());
-                            progress = 3 / progressTotal * 100;
-                            $('.ipt .progress-bar').css('width', progress + '%');
-                            assignDetailLevelSelected();
-                            styleRadio();
                         });
 
                         function updatePointers(direction) {
@@ -164,7 +103,7 @@ const questionnaireEndTemplate = require('./questionnaire-end.hbs');
                                             questions = iterations.length > 0 ? _.filter(iterations[iterationPointer]._embedded.questions, function(question) {
                                                 return question.detailLevel <= result.data.detailLevel;
                                             }) : [];
-                                            if (categories[categoryPointer].isRepeatable === true) {
+                                            if (categories[categoryPointer].isRepeatable) {
                                                 questionPointer = -1;
                                             }
                                         }
@@ -180,7 +119,7 @@ const questionnaireEndTemplate = require('./questionnaire-end.hbs');
                                     questionPointer += 1;
                                 }
                             } else if (direction === 'back') {
-                                if ((categories[categoryPointer].isRepeatable === true && iterationPointer === 0 && questionPointer - 1 < -1) || (categories[categoryPointer].isRepeatable === true && iterationPointer > 0 && questionPointer - 1 < 0) || (categories[categoryPointer].isRepeatable === false && questionPointer - 1 < 0)) {
+                                if ((categories[categoryPointer].isRepeatable && iterationPointer === 0 && questionPointer - 1 < -1) || (categories[categoryPointer].isRepeatable && iterationPointer > 0 && questionPointer - 1 < 0) || (!categories[categoryPointer].isRepeatable && questionPointer - 1 < 0)) {
                                     if (iterationPointer - 1 < 0) {
                                         if (categoryPointer - 1 < 0) {
                                             outOfBounds = 'front';
@@ -241,28 +180,61 @@ const questionnaireEndTemplate = require('./questionnaire-end.hbs');
                             return values;
                         }
 
+                        function introTemplateValues() {
+                            const currentCategory = _.find(result.data._embedded.questionnaires[0]._embedded.categories, (category) => {
+                                return category.id === categories[categoryPointer].id;
+                            });
+                            const currentQuestion = _.cloneDeep(_.find(currentCategory._embedded.questions, (question) => {
+                                return question.id === questions[questionPointer].id;
+                            }));
+                            let values = {category: currentCategory, question: currentQuestion};
+                            if (questionPointer) {
+                                values.notFirst = true;
+                            }
+
+                            if (typeof currentQuestion._embedded.images[0].url !== 'undefined') {
+                                values.image = currentQuestion._embedded.images[0].url;
+                            }
+
+                            return values;
+                        }
+
                         function updateQuestionContent(outOfBounds = '') {
-                            progress = (categoryPointer + 5) / progressTotal * 100;
+                            progress = categoryPointer / progressTotal * 100;
+                            const currentCategory = _.find(result.data._embedded.questionnaires[0]._embedded.categories, (category) => {
+                                return category.id === categories[categoryPointer].id;
+                            });
 
                             if (outOfBounds !== '') {
                                 if (outOfBounds === 'front') {
-                                    progress = 4 / progressTotal * 100;
-                                    $('.ipt-body').html(questionnaireSolutionCanvasTemplate());
+                                    // progress = 0
+                                    // $('.ipt-body').html(questionnaireSolutionCanvasTemplate());
                                 } else if (outOfBounds === 'end') {
                                     progress = progressTotal / progressTotal * 100;
                                     $('.ipt-body').html(questionnaireEndTemplate());
                                 }
                             } else if (questionPointer === -1) {
-                                const currentCategory = _.find(result.data._embedded.questionnaires[0]._embedded.categories, (category) => {
-                                    return category.id === categories[categoryPointer].id;
-                                });
                                 let image = '';
                                 if (currentCategory._embedded.images.length > 0) {
                                     image = currentCategory._embedded.images[0].url;
                                 }
                                 $('.ipt-body').html(questionnaireIterationTemplate({category: currentCategory, iterations: categories[categoryPointer]._embedded.iterations, image: image}));
                             } else {
-                                $('.ipt-body').html(questionnaireTemplate(templateValues()));
+                                if (currentCategory.name === 'intro') {
+                                    const currentQuestion = _.cloneDeep(_.find(currentCategory._embedded.questions, (question) => {
+                                        return question.id === questions[questionPointer].id;
+                                    }));
+                                    if (currentQuestion.name === 'Project Description') {
+                                        $('.ipt-body').html(questionnaireDescriptionTemplate({projectName: result.data.projectName, projectStatus: result.data.projectStatus, mainContact: result.data.mainContact, question: currentQuestion}));
+                                    } else if (currentQuestion.name === 'Levels of Detail') {
+                                        $('.ipt-body').html(questionnaireLevelsTemplate({level: result.data.detailLevel, question: currentQuestion}));
+                                        assignDetailLevelSelected();
+                                    } else {
+                                        $('.ipt-body').html(questionnaireIntroTemplate(introTemplateValues()));
+                                    }
+                                } else {
+                                    $('.ipt-body').html(questionnaireTemplate(templateValues()));
+                                }
                             }
 
                             $('.ipt .progress-bar').css('width', progress + '%');
@@ -283,45 +255,33 @@ const questionnaireEndTemplate = require('./questionnaire-end.hbs');
                             questions[questionPointer].answer = $("input[name='question-options'][checked='checked']").val();
                         }
 
-                        $(document).on('click', '.solution-canvas-next', (event) => {
-                            event.preventDefault();
-                            categoryPointer = 0;
-                            iterationPointer = 0;
-                            questionPointer = 0;
-                            progress = 5 / progressTotal * 100;
-                            $('.ipt .progress-bar').css('width', progress + '%');
+                        if (result.data._embedded.answers[0]._embedded.categories[categoryPointer].isRepeatable === true) {
+                            questionPointer = -1;
+                        }
 
-                            if (result.data._embedded.answers[0]._embedded.categories[categoryPointer].isRepeatable === true) {
-                                questionPointer = -1;
-                            }
-
-                            categories = result.data._embedded.answers[0]._embedded.categories;
-                            iterations = _.filter(categories[categoryPointer]._embedded.iterations, function(iteration) {
-                                return categories[categoryPointer].isRepeatable ? iteration.name !== '' : true;
-                            });
-                            questions = iterations.length > 0 ? _.filter(iterations[iterationPointer]._embedded.questions, function(question) {
-                                return question.detailLevel <= result.data.detailLevel;
-                            }) : [];
-
-                            updateQuestionContent();
+                        categories = result.data._embedded.answers[0]._embedded.categories;
+                        iterations = _.filter(categories[categoryPointer]._embedded.iterations, function(iteration) {
+                            return categories[categoryPointer].isRepeatable ? iteration.name !== '' : true;
                         });
-                        $(document).on('click', '.question-next', (event) => {
-                            event.preventDefault();
+                        questions = iterations.length > 0 ? _.filter(iterations[iterationPointer]._embedded.questions, function(question) {
+                            return question.detailLevel <= result.data.detailLevel;
+                        }) : [];
+
+                        updateQuestionContent();
+
+                        $(document).on('click', '.question-next', () => {
                             updateQuestions();
                             updatePointers('next');
                         });
-                        $(document).on('click', '.question-back', (event) => {
-                            event.preventDefault();
+                        $(document).on('click', '.question-back', () => {
                             updateQuestions();
                             updatePointers('back');
                         });
-                        $(document).on('click', '.iteration-next', (event) => {
-                            event.preventDefault();
+                        $(document).on('click', '.iteration-next', () => {
                             updateIterations();
                             updatePointers('next');
                         });
-                        $(document).on('click', '.iteration-back', (event) => {
-                            event.preventDefault();
+                        $(document).on('click', '.iteration-back', () => {
                             updateIterations();
                             updatePointers('back');
                         });
