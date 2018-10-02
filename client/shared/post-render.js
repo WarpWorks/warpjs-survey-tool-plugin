efcaa7eaf0dbf9e1961c9de1ed85fb2d6fdaff4f
Hữu Ðà Trần <huuda.tran@quoininc.com>
2018-10-02 16:06:50 -0400

    - #35: Adding surveyId and assessmentId to placeholder

--- client/shared/post-render.js
+++ client/shared/post-render.js
@@ -5,6 +5,8 @@ module.exports = ($, data) => {
     placeholder.data('surveyToolUrl', data._links.self.href);
     placeholder.data('surveyToolAssessmentTemplateUrl', data._links.assessmentTemplate.href);
     placeholder.data('surveyToolDefaultSurveyId', data.defaultSurveyId);
+    placeholder.data('surveyId', data.surveyId);
+    placeholder.data('assessmentId', data.assessmentId);
 
     $('[data-toggle="tooltip"]', placeholder).tooltip({
         container: 'body',
