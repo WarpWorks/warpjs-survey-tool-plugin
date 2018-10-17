f01f96e80ce5b4ff8e20da98f554b75084056afd
mcounts <mcounts@quoininc.com>
2018-10-17 14:28:19 -0400

    add missing version label

--- client/assessment/get-version.js
+++ client/assessment/get-version.js
@@ -3,7 +3,7 @@ module.exports = (assessment) => {
     if (assessment._meta && assessment._meta.history && assessment._meta.history.length) {
         version = 'version ' + assessment._meta.history[assessment._meta.history.length - 1].revision;
     } else if (assessment.exportProperties) {
-        version = assessment.exportProperties.revision;
+        version = 'version ' + assessment.exportProperties.revision;
     } else {
         version = '';
     }
