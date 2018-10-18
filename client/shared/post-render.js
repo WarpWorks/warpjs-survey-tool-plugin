e9908b2ed13529ec38ab411bf654861e558692de
mcounts <mcounts@quoininc.com>
2018-10-18 14:02:19 -0400

    #54 link logo to homepage

--- client/shared/post-render.js
+++ client/shared/post-render.js
@@ -6,12 +6,13 @@ module.exports = ($, data) => {
     const placeholder = $('#warpjs-content-placeholder');
 
     storage.setCurrent($, 'warpjsUser', data.warpjsUser);
-
     storage.setCurrent($, 'surveyToolUrl', data._links.self.href);
     storage.setCurrent($, 'surveyToolAssessmentTemplateUrl', data._links.assessmentTemplate.href);
     storage.setCurrent($, 'surveyToolDefaultSurveyId', data.defaultSurveyId);
     storage.setCurrent($, 'surveyId', data.surveyId);
     storage.setCurrent($, 'assessmentId', data.assessmentId);
+
+    $('.warpjs-home-link').attr('href', data._links.warpjsHomepage.href);
 
     $('[data-toggle="tooltip"]', placeholder).tooltip({
         container: 'body',
