fa19de9aeea1dbdbbe9c5aed4e1f9fb73ccf6007
Matt Counts <mcounts@quoininc.com>
2019-03-15 13:40:18 -0400

    circular spider diagram demo working

--- client/assessment/index.js
+++ client/assessment/index.js
@@ -1061,52 +1061,49 @@ const d3 = require('d3');
                                                 ]
                                             };
 
-                                            var svg = d3.select("svg");
-                                            var width = +svg.attr("width");
-                                            const tree = data => {
+                                            const width = 932;
+                                            const radius = width / 2;
+                                            // const tree = (data) => {
+                                            //     d3.tree()
+                                            //         .size([2 * Math.PI, radius])
+                                            //         .separation((a, b) => (a.parent === b.parent ? 1 : 2) / a.depth);
+                                            //     return d3.hierarchy(data);
+                                            // };
+
+                                            const tree = (data) => {
                                                 const root = d3.hierarchy(data);
-                                                root.dx = 10;
-                                                root.dy = width / (root.height + 1);
-                                                return d3.tree().nodeSize([root.dx, root.dy])(root);
+                                                return d3.tree().size([2 * Math.PI, radius]).separation((a, b) => (a.parent === b.parent ? 1 : 2) / a.depth)(root);
                                             };
 
-                                            const root = tree(data);
+                                            const root1 = tree(data);
+                                            console.log('root1', root1);
 
-                                            let x0 = Infinity;
-                                            let x1 = -x0;
-                                            root.each(d => {
-                                                if (d.x > x1) {
-                                                    x1 = d.x;
-                                                }
-                                                if (d.x < x0) {
-                                                    x0 = d.x;
-                                                }
-                                            });
+                                            const svg = d3.select('svg.spider');
 
-                                            const g = svg.append("g")
-                                                .attr("font-family", "sans-serif")
-                                                .attr("font-size", 10)
-                                                .attr("transform", `translate(${root.dy / 3},${root.dx - x0})`);
+                                            const autosize = function(svg) {
+                                                const box = svg.getBBox();
+                                                svg.setAttribute("viewBox", `${box.x} ${box.y} ${box.width} ${box.height}`);
+                                            };
 
-                                            g.append("g")
+                                            svg.append("g")
                                                 .attr("fill", "none")
                                                 .attr("stroke", "#555")
                                                 .attr("stroke-opacity", 0.4)
                                                 .attr("stroke-width", 1.5)
                                                 .selectAll("path")
-                                                .data(root.links())
+                                                .data(root1.links())
                                                 .join("path")
-                                                .attr("d", d3.linkHorizontal()
-                                                    .x(d => d.y)
-                                                    .y(d => d.x));
+                                                .attr("d", d3.linkRadial()
+                                                    .angle(d => d.x)
+                                                    .radius(d => d.y));
 
-                                            const node = g.append("g")
+                                            const node = svg.append("g")
                                                 .attr("stroke-linejoin", "round")
                                                 .attr("stroke-width", 3)
                                                 .selectAll("g")
-                                                .data(root.descendants())
+                                                .data(root1.descendants().reverse())
                                                 .join("g")
-                                                .attr("transform", d => `translate(${d.y},${d.x})`);
+                                                .attr("transform", d => `rotate(${d.x * 180 / Math.PI - 90})translate(${d.y},0)`);
 
                                             node.append("circle")
                                                 .attr("fill", d => d.children ? "#555" : "#999")
@@ -1114,13 +1111,14 @@ const d3 = require('d3');
 
                                             node.append("text")
                                                 .attr("dy", "0.31em")
-                                                .attr("x", d => d.children ? -6 : 6)
-                                                .attr("text-anchor", d => d.children ? "end" : "start")
+                                                .attr("x", d => (d.x < Math.PI) === !d.children ? 6 : -6)
+                                                .attr("text-anchor", d => (d.x < Math.PI) === !d.children ? "start" : "end")
+                                                .attr("transform", d => d.x >= Math.PI ? "rotate(180)" : null)
                                                 .text(d => d.data.name)
                                                 .clone(true).lower()
                                                 .attr("stroke", "white");
 
-                                            svg.node();
+                                            autosize(svg.node());
 
                                             assignDetailLevelSelected();
                                         } else {
