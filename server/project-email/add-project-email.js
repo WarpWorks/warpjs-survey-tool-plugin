88674e2f9f5725152c640fdd6d9a245533937289
Hữu Ðà Trần <huuda.tran@quoininc.com>
2020-07-15 03:00:06 -0400

    - #173: Saving information in DB. Still need to be able to see it.

--- server/project-email/add-project-email.js
+++ server/project-email/add-project-email.js
@@ -1,15 +1,61 @@
+const constants = require('./../../lib/constants');
 const utils = require('./../utils');
+
+const ENTITY_NAME = 'IPT_ProjectEmail';
+const SURVEY_TOOL_RELATIONSHIP_NAME = 'SurveyTool';
 
 module.exports = async (req, res) => {
     const { surveyId } = req.params;
-    console.log('surveyId=', surveyId);
     const { projectEmail } = req.body;
-    console.log('projectEmail=', projectEmail);
 
-    const pluginInfo = utils.getPluginInfo(req);
-    const domain = pluginInfo.domain;
+    const email = projectEmail ? projectEmail.trim() : '';
 
-    console.log('domain=', domain);
+    let persistence;
 
-    res.status(204).send();
+    try {
+        const pluginInfo = utils.getPluginInfo(req);
+        const domain = pluginInfo.domain;
+
+        const pluginConfig = req.app.get(constants.appKeys.pluginConfig);
+        const Persistence = require(pluginConfig.persistence.module);
+        persistence = new Persistence(pluginConfig.persistence.host, domain);
+
+        const domainModel = await req.app.get(constants.appKeys.warpCore).getDomainByName(domain);
+        const projectEmailEntity = domainModel.getEntityByName(ENTITY_NAME);
+
+        const documents = await projectEmailEntity.getDocuments(persistence);
+        const found = documents.find((doc) => doc.Name === email);
+
+        const relationship = projectEmailEntity.getRelationshipByName(SURVEY_TOOL_RELATIONSHIP_NAME);
+        const surveyDocuments = await relationship.getTargetEntity().getDocuments(persistence);
+        const survey = surveyDocuments.find((survey) => survey.id === surveyId);
+        const surveyData = {
+            id: surveyId,
+            type: survey.type,
+            typeID: survey.typeID
+        };
+
+        if (found) {
+            const reference = relationship.getTargetReferences(found).find((ref) => ref._id === surveyId);
+            if (!reference) {
+                const instance = await relationship.addAssociation(found, surveyData, persistence);
+                await projectEmailEntity.updateDocument(persistence, instance);
+            }
+        } else {
+            let instance = projectEmailEntity.newInstance();
+            instance.Name = email;
+            instance = await relationship.addAssociation(instance, surveyData, persistence);
+            await projectEmailEntity.createDocument(persistence, instance);
+        }
+
+        res.status(204).send();
+    } catch (err) {
+        // eslint-disable-next-line no-console
+        console.error("Error adding project email: err=", err);
+        res.status(500).send('Error saving email.');
+    } finally {
+        if (persistence) {
+            persistence.close();
+        }
+    }
 };
